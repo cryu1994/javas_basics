db0a767d8c73cee427f8e1aecda96caab2e0ce28
Home <Home@Homes-MacBook-Pro-2.local>
2017-03-27 18:27:06 -0700

    qw

---
+++ app.js
@@ -0,0 +1,11 @@
+function rewards(){
+    for (var i =1; i <= 30; i++){
+        var day = " ";
+        for(var k = i; k <= i.length; k * 0.01){
+            day += ((k * i) + " ");
+        }
+        console.log(day)
+
+    }
+}
+rewards();
